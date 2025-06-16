1f54ec7456f4c7c3a07ced479e9b61501ffca95c
Arthur Franz <arthurfranzmoura@gmail.com>
2025-06-16 13:12:40 -0300

    slide and change login screen for registration

--- public/components/iconsHover.js
+++ public/components/iconsHover.js
@@ -1,35 +1,44 @@
 export default function iconsHover() {
-  const facebookIcon = document.querySelector('.facebook-icon');
-  const googleIcon = document.querySelector('.google-icon');
-  const linkedinIcon = document.querySelector('.linkedin-icon');
+  const facebookIcons = document.querySelectorAll('.facebook-icon');
+  const googleIcons = document.querySelectorAll('.google-icon');
+  const linkedinIcons = document.querySelectorAll('.linkedin-icon');
 
-  facebookIcon.addEventListener('mouseover', () => {
-    const icon = facebookIcon.querySelector("img");
-    icon.src = "./img/dark-facebook-icon.png";
-  });
+  // hover em todos os ícones do facebook.
+  facebookIcons.forEach(icon => {
+    const img = icon.querySelector('img');
+    
+    icon.addEventListener('mouseover', () => {
+      img.src = "./img/dark-facebook-icon.png";
+    });
 
-  facebookIcon.addEventListener('mouseleave', () => {
-    const icon = facebookIcon.querySelector("img");
-    icon.src = "./img/facebook-icon.png";
+    icon.addEventListener('mouseleave', (event) => {
+      img.src = "./img/facebook-icon.png";
+    });
   });
   
-  googleIcon.addEventListener('mouseover', () => {
-    const icon = googleIcon.querySelector("img");
-    icon.src = "./img/dark-google-icon.png";
+  // hover em todos os ícones do google.
+  googleIcons.forEach(icon => {
+    const img = icon.querySelector('img');
+
+    icon.addEventListener('mouseover', (event) => {
+      img.src = "./img/dark-google-icon.png";
+    });
+
+    icon.addEventListener('mouseleave', (event) => {
+      img.src = "./img/google-icon.png";
+    });
   });
 
-  googleIcon.addEventListener('mouseleave', () => {
-    const icon = googleIcon.querySelector("img");
-    icon.src = "./img/google-icon.png";
-  });
+  // hover em todos os ícones do linkedin.
+  linkedinIcons.forEach(icon => {
+    const img = icon.querySelector('img');
 
-  linkedinIcon.addEventListener('mouseover', () => {
-    const icon = linkedinIcon.querySelector("img");
-    icon.src = "./img/dark-linkedin-icon.png";
-  });
+    icon.addEventListener('mouseover', (event) => {
+      img.src = "./img/dark-linkedin-icon.png";
+    });
 
-  linkedinIcon.addEventListener('mouseleave', () => {
-    const icon = linkedinIcon.querySelector("img");
-    icon.src = "./img/linkedin-icon.png";
+    icon.addEventListener('mouseleave', (event) => {
+      img.src = "./img/linkedin-icon.png";
+    });
   });
 }
--- public/components/slideControl.js
+++ public/components/slideControl.js
@@ -1,22 +1,53 @@
 export default function slideControl() {
   const signInBtn = document.querySelector('.sign-in-btn');
   const signUpBtn = document.querySelector('.sign-up-btn');
-  const container = document.querySelector('.container');
-  const form = document.querySelector('.form-container');
-  const welcome = document.querySelector('.welcome-container');
 
+  const container = document.querySelector('.container');
+
+  const formSignIn = document.querySelector('.sign-in-container');
+  const formSignUp = document.querySelector('.sign-up-container');
+  const welcomeSignIn = document.querySelector('.welcome-sign-in');
+  const welcomeSignUp = document.querySelector('.welcome-sign-up');
+
+  // mostra o form de login
+  const showSignIn = () => {
+    formSignUp.classList.add('hide');
+    welcomeSignUp.classList.add('hide');
+    formSignIn.classList.remove('hide');
+    welcomeSignIn.classList.remove('hide');
+  }
+
+  // mostra o form de register
+  const showSignUp = () => {
+    formSignIn.classList.add('hide');
+    welcomeSignIn.classList.add('hide');
+    formSignUp.classList.remove('hide');
+    welcomeSignUp.classList.remove('hide');
+  }
+
+  // ao clicar no botão de sign in.
   signInBtn.addEventListener('click', () => {
     container.classList.add('active');
-    form.classList.add('hidden');
-    welcome.classList.add('hidden');
+    formSignUp.classList.add('hidden');
+    welcomeSignUp.classList.add('hidden');
 
     setTimeout(() => {
-      form.classList.remove('hidden');
-      welcome.classList.remove('hidden');
-    }, 550);
+      formSignUp.classList.remove('hidden');
+      welcomeSignUp.classList.remove('hidden');
+      showSignIn();
+    }, 520);
   });
 
+  // ao clicar no botão de sign up.
   signUpBtn.addEventListener('click', () => {
     container.classList.remove('active');
+    formSignIn.classList.add('hidden');
+    welcomeSignIn.classList.add('hidden');
+
+    setTimeout(() => {
+      formSignIn.classList.remove('hidden');
+      welcomeSignIn.classList.remove('hidden');
+      showSignUp();
+    }, 550);
   });
 }

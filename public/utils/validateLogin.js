32c8232ba8131e8a4c928216b57ad926c5b859af
Arthur Franz <arthurfranzmoura@gmail.com>
2025-07-15 13:13:31 -0300

    any bugs fixed

--- public/utils/validateLogin.js
+++ public/utils/validateLogin.js
@@ -7,10 +7,6 @@ export default function validateLogin() {
   const inputEmail = document.querySelector('.input-email-login');
   const inputPassword = document.querySelector('.input-password-login');
   
-  // texto de erros.
-  const emailError = document.querySelector('.input-email-login .input-error');
-  const passwordError = document.querySelector('.input-password-login .input-error');
-  
   // valor dos inputs.
   const email = document.getElementById('email-login');
   const password = document.getElementById('password-login');
@@ -30,58 +26,44 @@ export default function validateLogin() {
   // limpa os erros ao começar a digitar.
   const isTyping = () => {
     const inputArea = document.querySelectorAll('.input-area');
-    const inputError = document.querySelectorAll('.input-error');
     const inputs = document.querySelectorAll('.input-area input');
 
     inputs.forEach(i => {
       if (i.value.length > 0) {
         inputArea.forEach(i => i.classList.remove('invalid'));
-        inputError.forEach(i => i.classList.remove('error'));
       }
     });
   }
 
   const isRegisteredEmail = () => {
     const user = users.find(user => user.email === email.value);
-    const error = emailError.querySelector('span');
     inputEmail.classList.add('invalid');
-    emailError.classList.add('error');
 
     if (email.value == "") {
-      error.textContent = "This field must not be empty.";
       return false;
     }
 
     if (!user) {
-      error.textContent = "This email isn't registered.";
       return false;
     }
 
     inputEmail.classList.remove('invalid');
-    emailError.classList.remove('error');
-    error.textContent = "";
     return true;
   }
 
   const isRegisteredPassword = () => {
     const user = users.find(user => user.email === email.value);
-    const error = passwordError.querySelector('span');
     inputPassword.classList.add('invalid');
-    passwordError.classList.add('error');
 
     if (password.value == "") {
-      error.textContent = "This field must not be empty.";
       return false;
     }
 
     if (!user || user.password != password.value) {
-      error.textContent = "Incorrect password.";
       return false;
     }
 
     inputPassword.classList.remove('invalid');
-    passwordError.classList.remove('error');
-    error.textContent = "";
     return true;
   }
 
--- public/utils/validateRegister.js
+++ public/utils/validateRegister.js
@@ -5,12 +5,6 @@ export default function validateRegister() {
   const inputEmail = document.querySelector('.input-email');
   const inputPassword = document.querySelector('.input-password');
   const inputConfirmPassword = document.querySelector('.input-confirm-password');
-
-  // texto de erro.
-  const usernameError = document.querySelector('.input-username .input-error');
-  const emailError = document.querySelector('.input-email .input-error');
-  const passwordError = document.querySelector('.input-password .input-error');
-  const confirmPasswordError = document.querySelector('.input-confirm-password .input-error');
   
   // valor dos inputs.
   const username = document.getElementById('username');
@@ -36,31 +30,24 @@ export default function validateRegister() {
   const isValidateUsername = () => {
     // pega todos os nomes de usuário já cadastrados.
     const registeredUsernames = users.map(user => user.username);
-    const error = usernameError.querySelector('span');
 
     inputUsername.classList.add('invalid');
-    usernameError.classList.add('error');
     
     // se estiver vazio.
     if (username.value == "") {
-      error.textContent = "This field must not be empty.";
       return false;
     }
 
     // se já incluso nos cadastros.
     if (registeredUsernames.includes(username.value)) {
-      error.textContent = "Username already registered.";
       return false;
     }
 
     if (!/^[a-zA-Z0-9._]+$/.test(username.value) || /\s/.test(username.value)) {
-      error.textContent = "Only letters, numbers, '.' and '_' allowed.";
       return false;
     }
 
     inputUsername.classList.remove('invalid');
-    usernameError.classList.remove('error');
-    error.textContent = "";
     return true;
   }
 
@@ -71,42 +58,33 @@ export default function validateRegister() {
 
     // pega todos os emails já cadastrados.
     const registeredEmails = users.map(user => user.email);
-    const error = emailError.querySelector('span');
 
     inputEmail.classList.add('invalid');
-    emailError.classList.add('error');
 
     // se estiver vazio.
-    if (email.value == "") {  
-      error.textContent = "This field must not be empty.";  
+    if (email.value == "") {   
       return false;
     }
 
     // se não passar no teste.
     if (!emailRegex.test(email.value) && email.value != "") {
-      error.textContent = "Invalid Email.";
       return false;
     }
 
     // se já incluso nos cadastros.
     if (registeredEmails.includes(email.value)) {
-      error.textContent = "Email already registered.";
       return false;
     }
 
     inputEmail.classList.remove('invalid');
-    emailError.classList.remove('error');
-    error.textContent = "";
     return true;
   };
   
   // validação da senha.
   const isValidatePassword = () => {
-    const error = passwordError.querySelector('span');
     let errors = [];
 
     inputPassword.classList.add('invalid');
-    passwordError.classList.add('error');
 
     // verifica se há número.
     if (!/[0-9]/.test(password.value)) errors.push("number");
@@ -119,42 +97,33 @@ export default function validateRegister() {
     
     // se estiver vazio.
     if (email.value == "") {
-      error.textContent = "This field must not be empty.";
       return false;
     }
 
     // se houver pelo um erro.
     if (errors.length > 0) {
-      error.textContent = `Missing: ${errors.join(", ")}`;
       return false;
     }
 
     inputPassword.classList.remove('invalid');
-    passwordError.classList.remove('error');
-    error.textContent = "";
     return true;
   }
 
   // verifica se as senhas são iguais.
   const equalPassword = () => {
-    const error = confirmPasswordError.querySelector('span');
     inputConfirmPassword.classList.add('invalid');
-    confirmPasswordError.classList.add('error');
 
     // se estiver vazio.
     if (confirmPassword.value == "") {
-      error.textContent = "This field must not be empty.";
       return false;
     }
 
     // se as senhas forem diferentes.
     if (password.value !== confirmPassword.value) {
-      error.textContent = "Passwords don't match.";
       return false;
     }
 
     inputConfirmPassword.classList.remove('invalid');
-    confirmPasswordError.classList.remove('error');
     return true;
   }
   
